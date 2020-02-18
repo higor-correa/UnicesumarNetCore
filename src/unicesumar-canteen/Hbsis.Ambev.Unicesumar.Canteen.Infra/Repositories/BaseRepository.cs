b1213b73a244df17a034859ad9abf93601033e21
Higor Correa <higor.correa@hbsis.com.br>
2020-02-18 20:38:01 -0300

    Added a base repository

---
+++ src/unicesumar-canteen/Hbsis.Ambev.Unicesumar.Canteen.Infra/Repositories/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using Hbsis.Ambev.Unicesumar.Canteen.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Hbsis.Ambev.Unicesumar.Canteen.Infra.Repositories
+{
+    public class BaseRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly DbContext _context;
+
+        public DbSet<TEntity> DbSet { get; set; }
+
+        public BaseRepository(DbContext dbContext)
+        {
+            _context = dbContext;
+            DbSet = dbContext.Set<TEntity>();
+        }
+
+        public async Task<TEntity> FindAsync(Guid id) => await DbSet.FirstOrDefaultAsync(x => x.Id == id);
+        public async Task AddAsync(TEntity entity) => await DbSet.AddAsync(entity);
+        public Task UpdateAsync(TEntity entity)
+        {
+            var isDetached = _context.Entry(entity).State == EntityState.Detached;
+            if (isDetached)
+            {
+                DbSet.Attach(entity);
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
